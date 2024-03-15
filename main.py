9d19243fe7e4b1fded193c01ab27b90e55175108
Elma Rudmila <17663780-ElmaRudmila1@users.noreply.replit.com>
2024-03-15 13:17:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+# Example code
+
+print("Hello World!")
+
+# My Prediction - Add your prediction as a comment below.
+ 
+ # I believe it will printout hello world.
+# my prediction was rigth when i printed it 
+
+##### Task Investigate
+
+# Which part of the example code is the output statement?
+ # Answer: hello world!
+
+# Which part of the example code is the string?
+ # Answer: print()
+
+# What would the output of the code print("I love Computing") be?
+ # Answer: the output of the code print would I love computing!
+
+# What would happen if the code print("I love Comping") was run?
+ # Answer: there will be an error because there are no parenthesis 
+
+# What would happen if the code print("I love Computing" was run?
+ # Answer: 
